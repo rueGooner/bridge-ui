e26a6cc7c4ad8393ed3f1f7386653e756477cede
Rueben <Rueben.cumberbatch@gamma.co.uk>
2025-06-20 20:46:22 +0100

    feat: clean up register method

--- server/routers/auth/register.ts
+++ server/routers/auth/register.ts
@@ -1,8 +1,6 @@
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import { baseProcedure, router } from "@/server/trpc";
 import { registerSchema } from "@/server/schemas/auth";
-import { serialize } from "cookie";
 
 const JWT_SECRET = process.env.JWT_SECRET_KEY!;
 
@@ -10,7 +8,12 @@ export const registerRouter = router({
   handleRegistration: baseProcedure
     .input(registerSchema)
     .mutation(async ({ input, ctx }) => {
-      const { password, email, passwordConfirmation, ...rest } = input;
+      const {
+        password: currentPassword,
+        email,
+        passwordConfirmation,
+        ...rest
+      } = input;
 
       const existingUser = await ctx.prisma.user.findUnique({
         where: {
@@ -20,7 +23,7 @@ export const registerRouter = router({
 
       if (existingUser) throw new Error("This User already exists");
 
-      const hashedPassword = await bcrypt.hash(password, 10);
+      const hashedPassword = await bcrypt.hash(currentPassword, 10);
       const newUser = await ctx.prisma.user.create({
         data: {
           email,
@@ -29,27 +32,13 @@ export const registerRouter = router({
         },
       });
 
-      const tokenPayload = {
-        userId: newUser.id,
-        email: newUser.email,
-        role: newUser.role,
+      const { password, ...user } = newUser;
+
+      return {
+        status: "Success",
+        data: {
+          user,
+        },
       };
-
-      const accessToken = jwt.sign(tokenPayload, JWT_SECRET, {
-        expiresIn: "1h",
-      });
-
-      ctx.res.setHeader(
-        "Set-Cookie",
-        serialize("token", accessToken, {
-          httpOnly: true,
-          secure: process.env.NODE_ENV === "production",
-          sameSite: "lax",
-          maxAge: 60 * 60, // 1 hour
-          path: "/",
-        })
-      );
-
-      return { id: newUser.id, email: newUser.email, accessToken };
     }),
 });
